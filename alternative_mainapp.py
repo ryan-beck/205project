ba02a82bfd42aeb5cca06b079354d3d39afcbea1
Torrdj <leo.tordjman@epita.fr>
2018-05-14 11:10:58 -0800

    add last changes from mainapp

--- alternative_mainapp.py
+++ alternative_mainapp.py
@@ -15,181 +15,179 @@ from shareButton import ShareButtonWindow
 import platform
 
 class ArtEngine(QMainWindow):
-	def __init__(self):
-		super().__init__()
-		
-		#Menu
-		if not platform.uname().system.startswith('Darw'):
-			self.mainMenu = self.menuBar()
+    def __init__(self):
+        super().__init__()
+        
+        #Menu
+        if not platform.uname().system.startswith('Darw'):
+            self.mainMenu = self.menuBar()
 
-			''' File
-				- Open
-				- Save
-			'''
-			openAct = QAction('Open', self)
-			openAct.triggered.connect(self.openButtonClicked)
+            ''' File
+                - Open
+                - Save
+            '''
+            openAct = QAction('Open', self)
+            openAct.triggered.connect(self.openButtonClicked)
 
-			saveAct = QAction('Save', self)
-			saveAct.triggered.connect(self.saveButtonClicked)
+            saveAct = QAction('Save', self)
+            saveAct.triggered.connect(self.saveButtonClicked)
 
-			fileMenu = self.mainMenu.addMenu('File')
-			fileMenu.addAction(openAct)
-			fileMenu.addAction(saveAct)
+            fileMenu = self.mainMenu.addMenu('File')
+            fileMenu.addAction(openAct)
+            fileMenu.addAction(saveAct)
 
-			''' Edit
-			'''
-			#editMenu = self.mainMenu.addMenu('Edit')
+            ''' Edit
+            '''
+            #editMenu = self.mainMenu.addMenu('Edit')
 
-			''' Tools
-			'''
-			shareAct = QAction('Share With Email', self)
-			shareAct.triggered.connect(self.shareButtonClicked)
-			
-			toolsMenu = self.mainMenu.addMenu('Tools')
-			toolsMenu.addAction(shareAct)
+            ''' Tools
+            '''
+            shareAct = QAction('Share With Email', self)
+            shareAct.triggered.connect(self.shareButtonClicked)
+            
+            toolsMenu = self.mainMenu.addMenu('Tools')
+            toolsMenu.addAction(shareAct)
 
-			''' Help
-			'''
-			#helpMenu = self.mainMenu.addMenu('Help')
+            ''' Help
+            '''
+            #helpMenu = self.mainMenu.addMenu('Help')
 
-		# ...
-		# Widget Creation
-		# ...
+        # ...
+        # Widget Creation
+        # ...
 
-		#self.appNameLabel = QLabel("Art Engine v0.01", self)
-		self.image = QLabel(self)
+        #self.appNameLabel = QLabel("Art Engine v0.01", self)
+        self.image = QLabel(self)
 
-		self.tca = AlgoOne(self.imgMan)
-		"""options = QFileDialog.Options()
-		options|= QFileDialog.DontUseNativeDialog
-		fileName, _ = QFileDialog.getOpenFileName(self,"Select Image", "","Image Files (*.png *.jpg)", options=options)
+        self.tca = AlgoOne(self.imgMan)
+        
+        self.openButtonClicked()
+        
+        layout = QVBoxLayout()
 
-		img = cv2.imread(fileName)
-		print("before", img.shape)
-		while(img.shape[0] > 300 or img.shape[1] > 1000):
-			img = cv2.resize(img,None,fx=9/10, fy=9/10, interpolation = cv2.INTER_CUBIC)
-		print("after", img.shape)
+        #Creating Buttons
 
-		self.gimg = img
+        filterNames = ["Select Filter", "Red x Negative", "Blue x Negative", "Green x Negative", "Erode x Morph", "Negative x Morph", "Dilate x Top Hat", "Morph x Top Hat", "Negate x Top Hat"]
+        buttonsLayout = QHBoxLayout()
+        
+        if platform.uname().system.startswith('Darw'):
+            self.openButton = QPushButton('Open', self)
+            self.saveButton = QPushButton('Save', self)
+            self.shareButton = QPushButton('Share With Email', self)
 
-		self.image.setPixmap(self.cvToPix(img))"""
-		self.openButtonClicked()
-		
-		layout = QVBoxLayout()
+            self.openButton.clicked.connect(self.openButtonClicked)
+            self.saveButton.clicked.connect(self.saveButtonClicked)
+            self.shareButton.clicked.connect(self.shareButtonClicked)
+        self.comboBox = QComboBox()
+        self.comboBox.addItems(filterNames)
 
-		#Creating Buttons
+        self.comboBox.currentIndexChanged.connect(self.applyFilterClicked)
+        
+        if platform.uname().system.startswith('Darw'):
+            buttonsLayout.addWidget(self.openButton)
+            buttonsLayout.addWidget(self.saveButton)
+            buttonsLayout.addWidget(self.shareButton)
+        buttonsLayout.addWidget(self.comboBox)
 
-		filterNames = ["Select Filter", "Red x Negative", "Blue x Negative", "Green x Negative", "Erode x Morph", "Negative x Morph", "Dilate x Top Hat", "Morph x Top Hat", "Negate x Top Hat"]
-		buttonsLayout = QHBoxLayout()
-		
-		if platform.uname().system.startswith('Darw'):
-			self.openButton = QPushButton('Open', self)
-			self.saveButton = QPushButton('Save', self)
-			self.shareButton = QPushButton('Share With Email', self)
+        layout.addLayout(buttonsLayout)
+        #layout.addWidget(self.appNameLabel)
+        layout.addWidget(self.image)
 
-			self.openButton.clicked.connect(self.openButtonClicked)
-			self.saveButton.clicked.connect(self.saveButtonClicked)
-			self.shareButton.clicked.connect(self.shareButtonClicked)
-		self.comboBox = QComboBox()
-		self.comboBox.addItems(filterNames)
+        self.firstLabel = QLabel("Choose Your Filter")
 
-		self.comboBox.currentIndexChanged.connect(self.applyFilterClicked)
-		
-		if platform.uname().system.startswith('Darw'):
-			buttonsLayout.addWidget(self.openButton)
-			buttonsLayout.addWidget(self.saveButton)
-			buttonsLayout.addWidget(self.shareButton)
-		buttonsLayout.addWidget(self.comboBox)
-
-		layout.addLayout(buttonsLayout)
-		#layout.addWidget(self.appNameLabel)
-		layout.addWidget(self.image)
-
-		self.firstLabel = QLabel("Choose Your Filter")
-
-		self.stacked_layout = QStackedLayout() # Creates stacked layout to hold the dial layouts for algos
-		self.stacked_layout.addWidget(self.firstLabel)
-		layout.addLayout(self.stacked_layout)
+        self.stacked_layout = QStackedLayout() # Creates stacked layout to hold the dial layouts for algos
+        self.stacked_layout.addWidget(self.firstLabel)
+        layout.addLayout(self.stacked_layout)
 
 
-		centralWidget = QWidget(self)
-		centralWidget.setLayout(layout)
-		self.setCentralWidget(centralWidget)
-		
-		self.setWindowTitle("ArtEngine v0.01")
-		self.setGeometry(100, 100, 300, 175)
-		self.imgMan()
-		self.show()
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(layout)
+        self.setCentralWidget(centralWidget)
+        
+        self.setWindowTitle("ArtEngine v0.01")
+        self.setGeometry(100, 100, 300, 175)
+        self.imgMan()
+        self.show()
 
-	def createAlgoLayout(self):
-		self.newLayout = self.tca.getModBox()
-		self.newWidget = QWidget()
-		self.newWidget.setLayout(self.newLayout)
-		self.stacked_layout.addWidget(self.newWidget)
+    def createAlgoLayout(self):
+        self.newLayout = self.tca.getModBox()
+        self.newWidget = QWidget()
+        self.newWidget.setLayout(self.newLayout)
+        self.stacked_layout.addWidget(self.newWidget)
 
-	def applyFilterClicked(self, value):
-		print(self.stacked_layout.count())
-		if value == 1:
-			self.tca = RedNeg(self.imgMan)
-		elif value == 2:
-			self.tca = BlueNeg(self.imgMan)
-		elif value == 3:
-			self.tca = GreenNeg(self.imgMan)
-		elif value == 4:
-			self.tca = ErodeMorph(self.imgMan)
-		elif value == 5:
-			self.tca = NegateMorph(self.imgMan)
-		elif value == 6:
-			self.tca = DilateTop(self.imgMan)
-		elif value == 7:
-			self.tca = MorphTop(self.imgMan)
-		elif value == 8:
-			self.tca = NegateTop(self.imgMan)
-		self.createAlgoLayout()
-		self.stacked_layout.setCurrentIndex(self.stacked_layout.count()-1)
+    def applyFilterClicked(self, value):
+        print(self.stacked_layout.count())
+        if value == 1:
+            self.tca = RedNeg(self.imgMan)
+        elif value == 2:
+            self.tca = BlueNeg(self.imgMan)
+        elif value == 3:
+            self.tca = GreenNeg(self.imgMan)
+        elif value == 4:
+            self.tca = ErodeMorph(self.imgMan)
+        elif value == 5:
+            self.tca = NegateMorph(self.imgMan)
+        elif value == 6:
+            self.tca = DilateTop(self.imgMan)
+        elif value == 7:
+            self.tca = MorphTop(self.imgMan)
+        elif value == 8:
+            self.tca = NegateTop(self.imgMan)
+        elif value == 9:
+            self.tca = BlurTop(self.imgMan)
+        elif value == 10:
+            self.tca = BlurNeg(self.imgMan)
+        self.createAlgoLayout()
+        self.stacked_layout.setCurrentIndex(self.stacked_layout.count()-1)
 
 
-	def openButtonClicked(self):
-		options = QFileDialog.Options()
-		options|= QFileDialog.DontUseNativeDialog
-		fileName, _ = QFileDialog.getOpenFileName(self,"Select Image", "","Image Files (*.png *.jpg)", options=options)
+    def openButtonClicked(self):
+        options = QFileDialog.Options()
+        options|= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"Select Image", "","Image Files (*.png *.jpg)", options=options)
 
-		img = cv2.imread(fileName)
-		while(img.shape[0] > 300 or img.shape[1] > 1000):
-			img = cv2.resize(img,None,fx=9/10, fy=9/10, interpolation = cv2.INTER_CUBIC)
+        img = cv2.imread(fileName)
+        while(img.shape[0] > 300 or img.shape[1] > 1000):
+            img = cv2.resize(img,None,fx=9/10, fy=9/10, interpolation = cv2.INTER_CUBIC)
 
-		self.gimg = img
+        self.gimg = img
 
-		self.image.setPixmap(self.cvToPix(img))
+        self.image.setPixmap(self.cvToPix(img))
 
-	def saveButtonClicked(self):
+    def saveButtonClicked(self):
 
-		w = len(self.final)
-		h = len(self.final[0])
+        w = len(self.final)
+        h = len(self.final[0])
 
-		cv2.imwrite("Images/lol.png", self.final)
-		print('Save Button Clicked')
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self,"Save Image","myImage.jpg","Image Files (*.png *.jpg)", options=options)
+        if fileName:
+            print(fileName)
 
-	def shareButtonClicked(self):
-		self.new_win = ShareButtonWindow()
+        cv2.imwrite(fileName, self.final)
+        print('Save Button Clicked')
 
-	# Converts an OpenCV image to PyQt5 image
-	def cvToPix(self, img):
-		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-		qImg = QImage(img.data, img.shape[1], img.shape[0], img.strides[0], QImage.Format_RGB888)
-		qpix = QPixmap()
-		qpix.convertFromImage(qImg)
-		return qpix
+    def shareButtonClicked(self):
+        self.new_win = ShareButtonWindow()
 
-	@pyqtSlot()
-	def imgMan(self):
+    # Converts an OpenCV image to PyQt5 image
+    def cvToPix(self, img):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        qImg = QImage(img.data, img.shape[1], img.shape[0], img.strides[0], QImage.Format_RGB888)
+        qpix = QPixmap()
+        qpix.convertFromImage(qImg)
+        return qpix
 
-		for op in self.tca.getOperators():
-			op.updateAll()
-		self.tca.updateRatios()
+    @pyqtSlot()
+    def imgMan(self):
 
-		self.final = self.tca.render(self.gimg)
-		self.image.setPixmap(self.cvToPix(self.final))
+        for op in self.tca.getOperators():
+            op.updateAll()
+        self.tca.updateRatios()
+
+        self.final = self.tca.render(self.gimg)
+        self.image.setPixmap(self.cvToPix(self.final))
 
 
 app = QApplication(sys.argv)
